5d81058476e7c45f3fa3eff46589d84e5e485589
Jose Centeno <centeno.jose.manuel@gmail.com>
2017-07-16 14:20:59 -0700

    fixed bug with initial timestamp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -113,6 +113,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     if (px_init < 0.0001 && py_init < 0.0001) return;
 
     ekf_.x_ << px_init, py_init, vx_init, vy_init;
+    previous_timestamp_ = measurement_pack.timestamp_;
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
